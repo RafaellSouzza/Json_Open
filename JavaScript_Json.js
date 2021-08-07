65df0de8677e1fb674ba839555f89769700e61a7
Rafael Souza <31011616+RafaellSouzza@users.noreply.github.com>
2021-08-07 16:58:53 -0300

    test

--- JavaScript_Json.js
+++ JavaScript_Json.js
@@ -1,22 +1,31 @@
 var salvar = (arquivo, objeto) => {
-    //Passando Objeto para Arquivo json JSON.stringify(objeto)//
-    const objetoJson = JSON.stringify(objeto);
-    //Metodo para abertura de arquivo require("fs")//
-    const fs = require("fs");
-    // Salvando arquivo de forma sincrona //
-    fs.writeFileSync(arquivo, objetoJson);
-    console.log("JSON data is saved.");
-  };
+  //Passando Objeto para Arquivo json JSON.stringify(objeto)//
+  const objetoJson = JSON.stringify(objeto);
+  //Metodo para abertura de arquivo require("fs")//
+  const fs = require("fs");
+  // Salvando arquivo de forma sincrona //
+  fs.writeFileSync(arquivo, objetoJson);
+  console.log("JSON data is saved.");
+};
 
-  var ler = (arquivo) => {
-    //Metodo para abertura de arquivo require("fs")//
-    const fs = require("fs");
-    //verifica se arquivo existe//
-    if (!fs.existsSync(arquivo)) return "Arquivo não existe.";
-    // lendo arquivo de forma sincrona //
-    const data = fs.readFileSync(arquivo, "utf-8");
-    // passando arquivo json pata objeto//
-    const objeto = JSON.parse(data.toString());
-    return objeto;
-  };
-  
+var ler = (arquivo) => {
+  //Metodo para abertura de arquivo require("fs")//
+  const fs = require("fs");
+  //verifica se arquivo existe//
+  if (!fs.existsSync(arquivo)) return "Arquivo não existe.";
+  // lendo arquivo de forma sincrona //
+  const data = fs.readFileSync(arquivo, "utf-8");
+  // passando arquivo json pata objeto//
+  const objeto = JSON.parse(data.toString());
+  return objeto;
+};
+
+console.log(ler("userx.json"));
+
+const user = {
+  id: 2,
+  name: "Rafael",
+  age: 24,
+};
+
+salvar("user.json", user);
